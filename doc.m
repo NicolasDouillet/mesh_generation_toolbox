a28c89dff132e58ae2c963952e622557b422f21c
NicolasDouillet <nicolas.douillet@free.fr>
2024-07-10 16:14:37 +0200

    Update doc + live script

--- doc.m
+++ doc.m
@@ -1,11 +1,10 @@
 %% mesh generation toolbox
 %
-% Author & support : nicolas.douillet (at) free.fr, 2020-2023.
-
-addpath('src/');
+% Author : nicolas.douillet (at) free.fr, 2020-2024.
 
 
 %% Example #1 : mesh an icosahedron based geoid
+addpath('src/');
 id = 4; % base = icosahedron
 nb_it = 3;
 sampling_mode = 'face';
@@ -19,14 +18,29 @@ plot_mesh(V,T);
 %% Example #3 : mesh an ovoid and select its face normals
 [V,T] = mesh_ovoid(8);
 select_face_normals(V,T); % beware : not all coherently oriented / facing outward
+alpha(1);
 
 %% Example #4 : mesh a torus with quasi equilateral triangles 
-longtude_nb_samples = 37;
+longitude_nb_samples = 37;
 latitude_nb_samples = 15; % odd number
-[V, T] = mesh_torus_specific(longtude_nb_samples,latitude_nb_samples);
+[V, T] = mesh_torus_specific(longitude_nb_samples,latitude_nb_samples);
 plot_mesh(V,T);
 
-%% Example #5 : (triangle) mesh a polygon
+%% Example #5 : mesh a torus with a density proportional to its curvature
+R = 9;
+r = 3;
+nb_samples = 3;
+[V, T] = mesh_torus_curv_adapt(R,r,nb_samples);
+plot_mesh(V,T);
+
+%% Example #6 first version of the torus 'isotropic' meshing algorithm
+R = 9;
+r = 3;
+nb_samples = 32;
+[V, T] = mesh_torus_isotropic(R,r,nb_samples);
+plot_mesh(V,T);
+
+%% Example #7 : (triangle) mesh a polygon
 % Hexagon of the 3D space
 V1 = [-sqrt(2)/3 sqrt(6)/3 -1/3];
 V3 = [-sqrt(2)/3 -sqrt(6)/3 -1/3];
@@ -38,7 +52,7 @@ P = cat(1,V1,V2,V3,V4,V5,V6);
 [V,T] = mesh_polygon(P,'sorted');
 plot_mesh(V,T);
 
-%% Example #6 : discrete contour mesh patch
+%% Example #8 : discrete contour mesh patch
 n = 32;
 V = 2*(rand(n,2)-0.5);
 H_raw = convhull(V);
